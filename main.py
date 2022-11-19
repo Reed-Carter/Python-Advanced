da05108ee1ddec11ba82579b0232a1e45777c463
Reed-Carter <reedandrew18@gmail.com>
2022-11-18 17:22:26 -0900

    remove_dups can accept a string as a parameter

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
+from typing import ByteString
 from faker import Faker
 import json
 
 fake = Faker()
 color_list = []
-color_list_no_dups = []
 
 for i in range(20):
   color_names = (fake.color_name())
   color_list.append(color_names)
 
-def remove_dups(list_of_colors):
-  [color_list_no_dups.append(color) for color in list_of_colors if color not in color_list_no_dups]
+def remove_dups(list_with_dups):
+  no_dups = []
+  [no_dups.append(color) for color in list_with_dups if color not in no_dups]
+  return "".join(no_dups) if isinstance(list_with_dups, str) else no_dups
 
-remove_dups(color_list)
+color_list_no_dups = remove_dups(color_list)
 color_dict = {color: len(color) for color in color_list_no_dups}
 
 with open("./data/colors.json", "w+") as json_colors:
@@ -24,3 +26,6 @@ with open("./data/colors.json", "r") as json_colors_short:
   for key, value in colors_short.items():
     if len(key) >= 4:
       print(f" The color {key} has {len(key)} letters in its name")
+
+
+
